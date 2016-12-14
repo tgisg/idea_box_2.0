36254a9b3e99c070779baf6412db99fda7ee5123
tgisg <soniagupta504@gmail.com>
2016-12-13 21:00:47 -0700

    added some fixes

--- app/controllers/api/v1/idea_controller.rb
+++ app/controllers/api/v1/idea_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::IdeaController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+  before_filter :cors_check
 
   def index
     @ideas = Idea.all
+    render json: @ideas
   end
 
   def create
     @idea = Idea.new(idea_params)
-    render :show, status: 201 if @idea.save
+    render json: @idea if @idea.save
   end
 
   def destroy
@@ -17,12 +20,19 @@ class Api::V1::IdeaController < ApplicationController
 
   def update
     @idea = Idea.find_by_id(params[:id])
-    render :show if @idea.update(idea_params)
+    render json: @idea if @idea.update(idea_params)
+  end
+
+  def cors_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
   private
 
   def idea_params
-    params.permit(:title, :body, :quality)
+    params.require(:idea).permit(:title, :body, :quality)
   end
 end
